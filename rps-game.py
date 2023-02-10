9bc73c1c640a8e5ef8f5bed169eab1b2b7df3dc3
hollynr <hollynreidy@gmail.com>
2023-02-10 11:03:47 -0700

    added comments

--- rps-game.py
+++ rps-game.py
@@ -1,16 +1,20 @@
 import pygame
 import random
-#import time
+#random is for computer choice, pygame is platform this runs thru
 pygame.init()
 
 #variables:
 BGCOLOR = "light blue"
+
+#user rps choice:
 player_1 = ""
+
+#score counters:
 tie = 0
 win = 0
 loss = 0
 
-#loading assets:
+#loading images:
 rock_image = pygame.image.load("assets/Rock.png")
 rock_image = pygame.transform.scale(rock_image, (100, 100))
 paper_image = pygame.image.load("assets/Paper.png")
@@ -19,16 +23,13 @@ scissors_image = pygame.image.load("assets/Scissors.png")
 scissors_image = pygame.transform.scale(scissors_image, (100, 100))
 
 
-
-#functions for interactable objects:
-
+#function to be able to show text:
 def printing_text(text, x, y):
         set_my_text = pygame.font.SysFont("arial", 60)
         textSurface = set_my_text.render(text, True, "black")
         screen.blit(textSurface, (x, y))
 
         
-
 #initialize screen
 screen = pygame.display.set_mode([1000, 1000])
 
@@ -40,6 +41,7 @@ while running:
             if event.type == pygame.QUIT:
                 running = False
                 '''
+                typing r, p, or s makes choice
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_r:
                     player_1 = "Rock"
@@ -48,6 +50,7 @@ while running:
                 elif event.key == pygame.K_s:
                     player_1 = "Scissors"
                     '''
+                # decision made if mouse touches one of the images while clicked
             if event.type == pygame.MOUSEBUTTONDOWN:
                     mouse_x, mouse_y = event.pos
                     if rock.collidepoint(mouse_x, mouse_y):
@@ -63,34 +66,28 @@ while running:
             
         # do stuff / proccess input
 
+        #makes computer decision:
         choices = ['Rock', 'Paper', 'Scissors']
         player_2 = choices[random.randint(0,2)]
         
-        
-
-
 
         # update screen
 
+        #refreshes screen
         screen.fill(BGCOLOR)
         rock = screen.blit(rock_image, (100,200))
         paper = screen.blit(paper_image, (350, 200))
         scissors = screen.blit(scissors_image, (600, 200))
         
+        #shows amount of ties, losses, and wins
         printing_text(f"ties: {tie} losses: {loss} wins: {win}", 200, 800)
         
+        #when user choice is made
         if player_1 != "":
 
                 printing_text(f"You chose {player_1}", 275, 500)
-                '''
-                pygame.time.wait(1000)
-                printing_text(".", 250, 600)
-                pygame.time.wait(1000)
-                printing_text(".", 500, 600)
-                pygame.time.wait(1000)
-                printing_text(".", 750, 600)
-                '''
                 printing_text(f"Your opponent chose {player_2}", 125, 600)
+
                 if player_1 == player_2:
                        printing_text("You tied", 400, 700)
                        tie += 1
@@ -117,18 +114,10 @@ while running:
                                 printing_text("You lost!", 400, 700)
                                 loss += 1
 
-
+                #gets rid of user decision so it doesn't keep playing over and over
                 pygame.display.update()
                 pygame.time.wait(1500)
                 player_1 = ""
-
-
-
-
-            
-
-        
-        #pygame.draw.circle(screen, "red", (400,300), 100)
     
         # draw screen
 
